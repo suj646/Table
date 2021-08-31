27e0369f36c1391d7a7c87f3d566d420a23da8d8
Sujit Bagade <sujitbagade2@gmail.com>
2021-08-31 23:07:05 +0530

    Table

---
+++ Tablenumber/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Tablenumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+                int num, product;
+                Console.Write("Enter the Number to Print its  Table: ");
+                num = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("\n Table For {0}: ", num);
+                for (int i = 1; i <= 10; i++)
+                {
+                    product = num * i;
+                    Console.WriteLine("{0} x {1} = {2}", num, i, product);
+                }
+
+               
+                Console.ReadKey();
+            }
+        }
+    }
+
